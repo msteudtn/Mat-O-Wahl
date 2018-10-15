a6dde4e7eed3d73694cb8389fe4e120f167c0451
msteudtn <msteudtn@htwm.de>
2018-10-15 14:43:39 +0200

    Create i18n_en.js

---
+++ i18n/i18n_en.js
@@ -0,0 +1,23 @@
+// ENGLISH LANGUAGE TEXTS http://www.mat-o-wahl.de
+// License: CC0 / Public Domain
+// Mathias Steudtner http://www.medienvilla.com
+
+// Buttons
+var TEXT_VOTING_PRO = "Vote for"
+var TEXT_VOTING_NEUTRAL = "No opinion"
+var TEXT_VOTING_CONTRA = "Vote against"
+var TEXT_VOTING_SKIP = "Skip (don't count)"
+var TEXT_VOTING_DOUBLE = "Very important for me"
+
+// Statistic
+var TEXT_ALLOW_STATISTIC = "Allow anonymous analytics (prognosis)? See <a href='http://"+imprintPrivacyUrl+"' target='_blank'>privacy statement</a>."
+
+// Footer
+var TEXT_IMPRINT = "Legal Notice"
+var TEXT_RESTART = "Restart questionnaire"
+
+// Results
+var TEXT_RESULTS_MATCHES_GENERAL = "Your match with the different positions"
+var TEXT_RESULTS_MATCHES_DETAILS = "Detailed analysis"
+var TEXT_RESULTS_MATCHES_DETAILS_INFO = "You can adjust your own opinion in the first row and reevaluate the result. <br /> Answers to the questions are shown on click on the corresponding question."
+var TEXT_RESULTS_MATCHES_DETAILS_TABLE = "Questions and answers of parties"
