0ace22bb789ee605f65c1c247dc755552b550ee0
msteudtn <msteudtn@htwm.de>
2020-01-27 08:38:39 +1300

    new version number

--- system/general.js
+++ system/general.js
@@ -3,7 +3,7 @@
 // License: GPL 3
 // Mathias Steudtner http://www.medienvilla.com
 
-var version = "0.3.0.20181103"
+var version = "0.4.0.20200125"
 
 // Globale Variablen
 var arQuestionsShort = new Array();	// Kurzform der Fragen: Atomkraft, Flughafenausbau, ...
