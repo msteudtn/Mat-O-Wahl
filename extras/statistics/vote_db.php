5dc88e0a32ba3e44cca085757628f7baddac37cb
Mathias <github.com@mars21.de>
2021-05-14 13:08:23 +0200

    Update vote_db.php

--- extras/statistics/vote_db.php
+++ extras/statistics/vote_db.php
@@ -1,40 +1,54 @@
 <?php
 
 // COMMIT: Marius Nisslmueller, Bad Honnef, Juni 2020
+// Anpassungen: Mathias Steudtner, Mai 2021
 
-// Send Data to Database
-// Database Settings
-$servername = "localhost";
-$username = "x";
-$password = "x";
-$dbname = "x";
+// Database Settings (Default for XAMPP :) )
+// $dbname has to be created once in your MySQL or MariaDB 
+// (and can be something else than "matowahl") 
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "matowahl";
 
 // Establish Connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+	if ($conn->connect_error) {
+	    die("Mat-o-Wahl: Connection failed: " . $conn->connect_error);
+	}
+
+// DE: IP-Adresse des Besuchers
+// EN: Ip address of visitor
+	if (getenv("HTTP_X_FORWARDED_FOR")) { 
+		$ip=getenv("HTTP_X_FORWARDED_FOR"); }
+	else {
+		$ip=getenv("REMOTE_ADDR"); }
+
+	$ip = explode (',', $ip);
+	$ip = $ip[0];
+	
+// Human readable Timestamp: e.g. "2021-12-31 23:59:59" 
+	$timestamp = date("Y-m-d H:i:s");
+
 
 // Sanitize User Input
-$mowPersonalValues = mysqli_real_escape_string($conn, $_GET["mowpersonal"]);
-$mowPartiesValues = mysqli_real_escape_string($conn, $_GET["mowparties"]);
+	$mowPersonalValues = mysqli_real_escape_string($conn, $_GET["mowpersonal"]);
+	$mowPartiesValues = mysqli_real_escape_string($conn, $_GET["mowparties"]);
 
 // Prepare and execute SQL Statement to prevent SQL Injection
-$sql = "INSERT INTO Results (ip, timest, personal, parties) VALUES (?,?,?,?)";
-if($query = $conn->prepare($sql)){
-    $query->bind_param('ssss',$ip, time(), $mowPersonalValues, $mowPartiesValues);
-    if ($query->execute() === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $query . "<br>" . $conn->error;
-    }
-} else {
-    $error = $conn->errno . ' ' . $conn->error;
-    echo $error;
-}
-  
-$conn->close();
+	$sql = "INSERT INTO Results (ip, timestamp, personal, parties) VALUES ('$ip', '$timestamp', '$mowPersonalValues', '$mowPartiesValues')";
+
+
+// Send data
+	if ($conn->query($sql) === TRUE) {
+	  echo "Mat-o-Wahl: New record created successfully";
+	} else {
+	  echo "Mat-o-Wahl: Error: " . $sql . "<br>" . $conn->error;
+	}
+
+// Close connection
+	$conn->close();
 
 ?>
