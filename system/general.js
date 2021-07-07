562c7a6f6fee015433ed2342bdc643e34306fd00
Mathias <github.com@mars21.de>
2021-07-07 19:54:46 +0200

    Update general.js

--- system/general.js
+++ system/general.js
@@ -3,7 +3,7 @@
 // License: GPL 3
 // Mathias Steudtner http://www.medienvilla.com
 
-var version = "0.6.0.20210702-BMBF-PTF-APx"
+var version = "0.6.0.20210707-BMBF-PTF-APx"
 
 // Globale Variablen
 var arQuestionsShort = new Array();	// Kurzform der Fragen: Atomkraft, Flughafenausbau, ...
