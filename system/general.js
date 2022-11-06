2db97df75636eae1d44f98b528217c9982b44ea6
Mathias <github.com@mars21.de>
2022-11-06 16:50:18 +0100

    Update version number

--- system/general.js
+++ system/general.js
@@ -3,7 +3,7 @@
 // License: GPL 3
 // Mathias Steudtner http://www.medienvilla.com
 
-var version = "0.6.0.5.20220827"
+var version = "0.6.0.5.20221106"
 
 // Globale Variablen
 var arQuestionsShort = new Array();	// Kurzform der Fragen: Atomkraft, Flughafenausbau, ...
