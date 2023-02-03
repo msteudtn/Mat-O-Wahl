92f044da689133229b07758a510f4ba7cfcd04b9
Felix Englisch <61431524+fenglisch@users.noreply.github.com>
2023-02-03 16:21:51 +0100

    Fokus nach jedem Klick entfernen
    ...damit die Hervorhebung der Buttons nicht bleibt
    
    Alternativ, damit das Event nicht nach jedem Klick überall im Dokument, sondern nur bei Buttons getriggert wird, ginge auch
    ```javascript
    document.querySelectorAll("button").forEach(button => {
       document.addEventListener("click", () => {
              document.activeElement.blur()
         }) 
    })
    ```
    aber ich weiß nicht, ob das groß einen Unterschied macht.
    
    Vermutlich gibt es dafür auch eine bessere Position im Code, mir ist nur auf die Schnelle nichts Besseres eingefallen.

--- system/output.js
+++ system/output.js
@@ -83,6 +83,11 @@ function fnStart()
 	$("#restart").attr("href","index.html?"+sekunden);
 	$("#restart").html(TEXT_RESTART);
 	
+	// Fokus nach jedem Klick entfernen
+	document.addEventListener("click", () => {
+          document.activeElement.blur()
+        }) 
+	
 	//////////////////////////////////////////////////////////////////
 	// FRAGEN UND ANTWORTEN in Arrays einlesen und Folgefunktionen aufrufen
 	// (a) Fragen 
