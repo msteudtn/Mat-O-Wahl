b1516931c6b0a0ff964d4b34449743c0b3a335be
Mathias <7988551+msteudtn@users.noreply.github.com>
2026-09-06 09:05:32 +0200

    Update i18n_de.js

--- i18n/i18n_de.js
+++ i18n/i18n_de.js
@@ -1,40 +1,52 @@
 // DEUTSCHE TEXTE http://www.mat-o-wahl.de
 
 // Buttons
-var TEXT_START = "Los geht's!"
-var TEXT_VOTING_PRO = "Stimme zu"
-var TEXT_VOTING_NEUTRAL = "Egal/Weiß nicht"
-var TEXT_VOTING_CONTRA = "Stimme nicht zu"
-var TEXT_VOTING_SKIP = "Überspringen"
-var TEXT_VOTING_DOUBLE = "Doppelt gewichten"
+const TEXT_START = "Los geht's!"
+const TEXT_VOTING_PRO = "Stimme zu"
+const TEXT_VOTING_NEUTRAL = "Egal/Weiß nicht"
+const TEXT_VOTING_CONTRA = "Stimme nicht zu"
+const TEXT_VOTING_SKIP = "Überspringen"
+const TEXT_VOTING_DOUBLE = "Doppelt gewichten"
+const TEXT_VOTING_FINISHED = "Ergebnisse zeigen!"
 
 // Statistic
-var TEXT_ALLOW_STATISTIC = "Anonyme Auswertung (Wahlprognose) erlauben. Siehe <a href='"+imprintPrivacyUrl+"' target='_blank'>Datenschutzerklärung</a>." // Variable loeschen!
+const TEXT_ALLOW_STATISTIC = "Anonyme Auswertung (Wahlprognose) erlauben. Siehe <a href='"+imprintPrivacyUrl+"' target='_blank'>Datenschutzerklärung</a>." // Variable loeschen!
 
-var TEXT_ALLOW_STATISTIC_TITLE = "Bevor dein Ergebnis erscheint ..."
-var TEXT_ALLOW_STATISTIC_TEXT = "Genehmigst du uns (den Betreibern dieser Seite) die Übertragung deiner <strong>anonymisierten</strong> Antworten für statistische Zwecke gemäß unserer <a href='"+imprintPrivacyUrl+"' target='_blank'>Datenschutzerklärung</a>? Damit hilfst du uns, den Mat-o-Wahl und die Fragen in Zukunft zu verbessern."
-var TEXT_ALLOW_STATISTIC_YES = "Ja, na klar!"
-var TEXT_ALLOW_STATISTIC_NO = "Nein, danke."
+const TEXT_ALLOW_STATISTIC_TITLE = "Bevor dein Ergebnis erscheint ..."
+const TEXT_ALLOW_STATISTIC_TEXT = "Genehmigst du uns (den Betreibern dieser Seite) die Übertragung deiner <strong>anonymisierten</strong> Antworten für statistische Zwecke gemäß unserer <a href='"+imprintPrivacyUrl+"' target='_blank'>Datenschutzerklärung</a>? Damit hilfst du uns, den Mat-o-Wahl und die Fragen in Zukunft zu verbessern."
+const TEXT_ALLOW_STATISTIC_YES = "Ja, na klar!"
+const TEXT_ALLOW_STATISTIC_NO = "Nein, danke."
 
 // Footer
-var TEXT_IMPRINT = "[§] Impressum"
-var TEXT_RESTART = "[&#x21BB;] Fragebogen neu starten"
+const TEXT_IMPRINT = "[§] Impressum"
+const TEXT_RESTART = "[&#x21BB;] Fragebogen neu starten"
 
 // Results
-var TEXT_RESULTS_HEADING = "Übereinstimmung mit den Positionen"
+const TEXT_RESULTS_CARD_SHORT = "Fertig"
+const TEXT_RESULTS_CARD_LONG = "Sie können sich nun Ihre Ergebnisse anzeigen lassen."
 
-var TEXT_RESULTS_INFO_THESES = "Sie können Ihre eigene Meinung in der zweiten Spalte noch einmal anpassen und das neue Ergebnis überprüfen. <br /> Die <strong>Antworten der Parteien</strong> erhalten Sie <strong>beim Klick auf das Plus-Zeichen</strong>."
-var TEXT_RESULTS_INFO_PARTIES = "Dies ist eine Übersicht aller Parteien und ihrer Antworten."
+const TEXT_RESULTS_HEADING_SHORT_SUMMARY = "Ergebnis"
 
-var TEXT_RESULTS_BUTTON_THESES = "Begründungen - Fragen und Antworten anzeigen"
-var TEXT_RESULTS_BUTTON_PARTIES= "Standpunkte - Parteien und Antworten anzeigen"
+const TEXT_RESULTS_TOGGLE_BUTTON = "Details zeigen / verbergen"
 
-// The following words may be used as ALT-Text or headers on the results-page 
-var TEXT_QUESTION = "Frage"
-var TEXT_POSITION_PARTY = "Position der Partei"
-var TEXT_ANSWER_PARTY = "Antwort der Partei"
-var TEXT_ANSWER_USER = "Ihre Antwort"
-var TEXT_IMAGE = "Logo oder Bild"
-var TEXT_PARTY = "Partei"
-var TEXT_ANSWER_NORMAL = "Frage einfach gewertet"
-var TEXT_ANSWER_DOUBLE = "Frage doppelt gewertet" 
+// 
+const TEXT_RESULTS_HEADING = "Übereinstimmung mit den Positionen"
+
+// 
+const TEXT_RESULTS_INFO_THESES = "Sie können Ihre eigene Meinung in der zweiten Spalte noch einmal anpassen und das neue Ergebnis überprüfen. <br /> Die <strong>Antworten der Parteien</strong> erhalten Sie <strong>beim Klick auf das Plus-Zeichen</strong>."
+const TEXT_RESULTS_INFO_PARTIES = "Dies ist eine Übersicht aller Parteien und ihrer Antworten."
+
+// 
+const TEXT_RESULTS_BUTTON_THESES = "Begründungen - Fragen und Antworten anzeigen"
+// 
+const TEXT_RESULTS_BUTTON_PARTIES= "Standpunkte - Parteien und Antworten anzeigen"
+
+// The following words may be used as ALT-Text or headers on the RESULTS-page 
+const TEXT_QUESTION = "Frage"
+const TEXT_POSITION_PARTY = "Position der Partei"
+const TEXT_ANSWER_PARTY = "Antwort der Partei"
+const TEXT_ANSWER_USER = "Ihre Antwort"
+const TEXT_IMAGE = "Logo oder Bild"
+const TEXT_PARTY = "Partei oder Kandidat"
+const TEXT_ANSWER_NORMAL = "Frage einfach gewertet"
+const TEXT_ANSWER_DOUBLE = "Frage doppelt gewertet" 
