2d320602ea53289b86f3e8ea8555a068aa163eb9
Mathias <7988551+msteudtn@users.noreply.github.com>
2026-09-10 19:10:15 +0200

    Update i18n_de.js

--- i18n/i18n_de.js
+++ i18n/i18n_de.js
@@ -1,52 +1,66 @@
 // DEUTSCHE TEXTE http://www.mat-o-wahl.de
 
-// Buttons
+// QUESTIONS - Buttons
 const TEXT_START = "Los geht's!"
-const TEXT_VOTING_PRO = "Stimme zu"
-const TEXT_VOTING_NEUTRAL = "Egal/Weiß nicht"
-const TEXT_VOTING_CONTRA = "Stimme nicht zu"
-const TEXT_VOTING_SKIP = "Überspringen"
-const TEXT_VOTING_DOUBLE = "Doppelt gewichten"
+const TEXT_VOTING_PRO_SHORT = "&#x2714;"
+const TEXT_VOTING_PRO_LONG  = "Stimme zu"
+const TEXT_VOTING_NEUTRAL_SHORT = "&#x25EF;" // "&#9900;" 
+const TEXT_VOTING_NEUTRAL_LONG  = "Egal/Weiß nicht"
+const TEXT_VOTING_CONTRA_SHORT =  "&#x2716;"
+const TEXT_VOTING_CONTRA_LONG  = "Stimme nicht zu"
+const TEXT_VOTING_SKIP_SHORT = "&#x21B7;"
+const TEXT_VOTING_SKIP_LONG  = "Überspringen"
+const TEXT_VOTING_DOUBLE_SHORT = "[x2]"
+const TEXT_VOTING_DOUBLE_LONG  = "Doppelt gewichten"
 const TEXT_VOTING_FINISHED = "Ergebnisse zeigen!"
 
-// Statistic
-const TEXT_ALLOW_STATISTIC = "Anonyme Auswertung (Wahlprognose) erlauben. Siehe <a href='"+imprintPrivacyUrl+"' target='_blank'>Datenschutzerklärung</a>." // Variable loeschen!
-
-const TEXT_ALLOW_STATISTIC_TITLE = "Bevor dein Ergebnis erscheint ..."
-const TEXT_ALLOW_STATISTIC_TEXT = "Genehmigst du uns (den Betreibern dieser Seite) die Übertragung deiner <strong>anonymisierten</strong> Antworten für statistische Zwecke gemäß unserer <a href='"+imprintPrivacyUrl+"' target='_blank'>Datenschutzerklärung</a>? Damit hilfst du uns, den Mat-o-Wahl und die Fragen in Zukunft zu verbessern."
-const TEXT_ALLOW_STATISTIC_YES = "Ja, na klar!"
-const TEXT_ALLOW_STATISTIC_NO = "Nein, danke."
-
-// Footer
-const TEXT_IMPRINT = "[§] Impressum"
-const TEXT_RESTART = "[&#x21BB;] Fragebogen neu starten"
-
-// Results
+// RESULTS (last card of the questions)
 const TEXT_RESULTS_CARD_SHORT = "Fertig"
 const TEXT_RESULTS_CARD_LONG = "Sie können sich nun Ihre Ergebnisse anzeigen lassen."
 
+// RESULTS - Overview (with the progress-bar and percentage)
 const TEXT_RESULTS_HEADING_SHORT_SUMMARY = "Ergebnis"
-
 const TEXT_RESULTS_TOGGLE_BUTTON = "Details zeigen / verbergen"
+const TEXT_ERROR_NO_DESCRIPTION = "Keine Beschreibung vorhanden."
+const TEXT_IMAGE = "Logo oder Bild"
 
-// 
+// RESULTS - Buttons to show more details
+const TEXT_RESULTS_BUTTON_QUESTIONS = "Begründungen - Fragen und alle Antworten anzeigen"
+const TEXT_RESULTS_BUTTON_CANDIDATES = "Standpunkte - Kandidaten und ihre Antworten anzeigen"
+
+
+// RESULTS - 
 const TEXT_RESULTS_HEADING = "Übereinstimmung mit den Positionen"
 
-// 
-const TEXT_RESULTS_INFO_THESES = "Sie können Ihre eigene Meinung in der zweiten Spalte noch einmal anpassen und das neue Ergebnis überprüfen. <br /> Die <strong>Antworten der Parteien</strong> erhalten Sie <strong>beim Klick auf das Plus-Zeichen</strong>."
-const TEXT_RESULTS_INFO_PARTIES = "Dies ist eine Übersicht aller Parteien und ihrer Antworten."
+const TEXT_RESULTS_BY_QUESTION_DESCRIPTION = "Sie können Ihre eigene Meinung hier noch einmal anpassen und das neue Ergebnis überprüfen. Die <strong>Antworten der Kandidaten</strong> erhalten Sie <strong>beim Klick auf das Dreieck</strong>."
+const TEXT_RESULTS_BY_CANDIDATE_DESCRIPTION = "Dies ist eine Übersicht aller Kandidaten und ihrer Antworten. Ihre eigene Antwort steht in der ersten Spalte beim Klick auf die Kandidaten."
 
-// 
-const TEXT_RESULTS_BUTTON_THESES = "Begründungen - Fragen und Antworten anzeigen"
-// 
-const TEXT_RESULTS_BUTTON_PARTIES= "Standpunkte - Parteien und Antworten anzeigen"
 
 // The following words may be used as ALT-Text or headers on the RESULTS-page 
 const TEXT_QUESTION = "Frage"
-const TEXT_POSITION_PARTY = "Position der Partei"
-const TEXT_ANSWER_PARTY = "Antwort der Partei"
-const TEXT_ANSWER_USER = "Ihre Antwort"
-const TEXT_IMAGE = "Logo oder Bild"
+// const TEXT_POSITION_PARTY = "Position der Partei"
+const TEXT_ANSWER_BY_USER = "Ihre Antwort"
+const TEXT_ANSWER_BY_CANDIDATE = "Antwort der Kandidaten"
+
+
 const TEXT_PARTY = "Partei oder Kandidat"
 const TEXT_ANSWER_NORMAL = "Frage einfach gewertet"
 const TEXT_ANSWER_DOUBLE = "Frage doppelt gewertet" 
+
+
+// Footer
+const TEXT_IMPRINT = "(§) Impressum"
+const TEXT_RESTART = "(&#x21BB;) Fragebogen neu starten"
+
+// MODAL - STATISTICS and ERROR "nothing answered"
+const TEXT_MODAL_STATISTIC_TITLE = "Bevor dein Ergebnis erscheint ..."
+const TEXT_MODAL_STATISTIC_TEXT = "Genehmigst du uns (den Betreibern dieser Seite) die Übertragung deiner <strong>anonymisierten</strong> Antworten für statistische Zwecke gemäß unserer <a href='"+imprintPrivacyUrl+"' target='_blank'>Datenschutzerklärung</a>? Damit hilfst du uns, den Mat-o-Wahl und die Fragen in Zukunft zu verbessern."
+const TEXT_MODAL_STATISTIC_YES = "Ja, na klar!"
+const TEXT_MODAL_STATISTIC_NO = "Nein, danke."
+
+const TEXT_MODAL_NOTHING_ANSWERED_TITLE = "Da stimmt was nicht!" 
+const TEXT_MODAL_NOTHING_ANSWERED_TEXT = "Es wurden keine Fragen beantwortet. Bitte beantworte erst einige Fragen um zur Auswertung zu gelangen."
+const TEXT_MODAL_NOTHING_ANSWERED_YES = "Wird erledigt!"
+const TEXT_MODAL_NOTHING_ANSWERED_NO = "Schließen"
+
+
