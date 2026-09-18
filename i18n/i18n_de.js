4d60b868eb2d52ac24a9aba6c8aa3bbcaf6dacc9
Mathias <7988551+msteudtn@users.noreply.github.com>
2026-09-18 14:46:46 +0200

    Add files via upload

--- i18n/i18n_de.js
+++ i18n/i18n_de.js
@@ -1,22 +1,39 @@
 // DEUTSCHE TEXTE http://www.mat-o-wahl.de
 
-// QUESTIONS - Buttons
+// const TEXT_LANGUAGE = "deutsch" // for future use / for a possible button to change languages
+
+// WELCOME
+const TEXT_SHARED_RESULTS = "Jemand hat Ihnen diesen Link zu seinen eigenen Ergebnissen geschickt. Sie können die Ergebnisse jetzt anschauen."
 const TEXT_START = "Los geht's!"
-const TEXT_VOTING_PRO_SHORT = "&#x2714;"
+
+// QUESTIONS - Buttons
+const TEXT_VOTING_PRO_EXTRA_SHORT = "&#x2714;" // Heavy Check Mark
+const TEXT_VOTING_PRO_EXTRA_LONG  = "Stimme sehr zu"
+
+const TEXT_VOTING_PRO_SHORT = "&#x1F5F8;" // Light Check Mark
 const TEXT_VOTING_PRO_LONG  = "Stimme zu"
-const TEXT_VOTING_NEUTRAL_SHORT = "&#x25EF;" // "&#9900;" 
+
+const TEXT_VOTING_NEUTRAL_SHORT = "&#x25EF;" // Large Circle, alternatively: "&#9900;" Medium Small White Circle
 const TEXT_VOTING_NEUTRAL_LONG  = "Egal/Weiß nicht"
-const TEXT_VOTING_CONTRA_SHORT =  "&#x2716;"
-const TEXT_VOTING_CONTRA_LONG  = "Stimme nicht zu"
+
+const TEXT_VOTING_CONTRA_SHORT =  "&#x2715;" // Multiplication X
+const TEXT_VOTING_CONTRA_LONG  = "Stimme nicht zu" 
+
+const TEXT_VOTING_CONTRA_EXTRA_SHORT =  "&#x2716;" // Heavy Multiplication X
+const TEXT_VOTING_CONTRA_EXTRA_LONG  = "Stimme gar nicht zu"
+
 const TEXT_VOTING_SKIP_SHORT = "&#x21B7;"
 const TEXT_VOTING_SKIP_LONG  = "Überspringen"
+
 const TEXT_VOTING_DOUBLE_SHORT = "[x2]"
 const TEXT_VOTING_DOUBLE_LONG  = "Doppelt gewichten"
+const TEXT_VOTING_DOUBLE_HELP  = "Ist dir eine Frage besonders wichtig? Dann klicke hier, bevor du deine Antwort auswählst.";
+
 const TEXT_VOTING_FINISHED = "Ergebnisse zeigen!"
 
 // RESULTS (last card of the questions)
 const TEXT_RESULTS_CARD_SHORT = "Fertig"
-const TEXT_RESULTS_CARD_LONG = "Sie können sich nun Ihre Ergebnisse anzeigen lassen."
+const TEXT_RESULTS_CARD_LONG = "Geschafft! Sie können sich nun Ihre Ergebnisse anzeigen lassen. Die Antworten können später auch noch einmal geändert werden."
 
 // RESULTS - Overview (with the progress-bar and percentage)
 const TEXT_RESULTS_HEADING_SHORT_SUMMARY = "Ergebnis"
@@ -32,25 +49,31 @@ const TEXT_RESULTS_BUTTON_CANDIDATES = "Standpunkte - Kandidaten und ihre Antwor
 // RESULTS - 
 const TEXT_RESULTS_HEADING = "Übereinstimmung mit den Positionen"
 
-const TEXT_RESULTS_BY_QUESTION_DESCRIPTION = "Sie können Ihre eigene Meinung hier noch einmal anpassen und das neue Ergebnis überprüfen. Die <strong>Antworten der Kandidaten</strong> erhalten Sie <strong>beim Klick auf das Dreieck</strong>."
-const TEXT_RESULTS_BY_CANDIDATE_DESCRIPTION = "Dies ist eine Übersicht aller Kandidaten und ihrer Antworten. Ihre eigene Antwort steht in der ersten Spalte beim Klick auf die Kandidaten."
+const TEXT_RESULTS_BY_QUESTION_DESCRIPTION = "Sie können Ihre eigene Meinung hier noch einmal anpassen und das neue Ergebnis überprüfen. Die <strong>Antworten der Kandidat(inn)en</strong> erhalten Sie <strong>beim Klick auf das Dreieck</strong>."
+const TEXT_RESULTS_BY_CANDIDATE_DESCRIPTION = "Dies ist eine Übersicht aller Kandidat(inn)en und ihrer Antworten. Ihre eigene Antwort steht in der ersten Spalte beim Klick auf die Kandidat(inn)en."
 
 
 // The following words may be used as ALT-Text or headers on the RESULTS-page 
 const TEXT_QUESTION = "Frage"
 // const TEXT_POSITION_PARTY = "Position der Partei"
 const TEXT_ANSWER_BY_USER = "Ihre Antwort"
-const TEXT_ANSWER_BY_CANDIDATE = "Antwort der Kandidaten"
+const TEXT_ANSWER_BY_CANDIDATE = "Antwort der Kandidat(inn)en"
 
 
-const TEXT_PARTY = "Partei oder Kandidat"
+const TEXT_PARTY = "Partei oder Kandidat/in"
 const TEXT_ANSWER_NORMAL = "Frage einfach gewertet"
 const TEXT_ANSWER_DOUBLE = "Frage doppelt gewertet" 
 
 
 // Footer
 const TEXT_IMPRINT = "(§) Impressum"
+const TEXT_SHARE = "&#x1F875; Teilen"
+const TEXT_SHARE_CLIPBOARD = "&#x1F875; in Zwischenablage"
+const TEXT_SHARE_EMAIL = "&#x1F875; per E-Mail"
+const TEXT_SHARE_EMAIL_SUBJECT = "Ich habe den Mat-o-Wahl ausprobiert. Mach es auch!"
+const TEXT_SHARE_EMAIL_BODY = "Hallo, %0DIch habe gerade den Mat-o-Wahl ausprobiert. Hier kannst du es auch tun: %0D"
 const TEXT_RESTART = "(&#x21BB;) Fragebogen neu starten"
+const TEXT_SCROLL_TO_TOP = "&#x1F871; nach oben"
 
 // MODAL - STATISTICS and ERROR "nothing answered"
 const TEXT_MODAL_STATISTIC_TITLE = "Bevor dein Ergebnis erscheint ..."
@@ -59,8 +82,11 @@ const TEXT_MODAL_STATISTIC_YES = "Ja, na klar!"
 const TEXT_MODAL_STATISTIC_NO = "Nein, danke."
 
 const TEXT_MODAL_NOTHING_ANSWERED_TITLE = "Da stimmt was nicht!" 
-const TEXT_MODAL_NOTHING_ANSWERED_TEXT = "Es wurden keine Fragen beantwortet. Bitte beantworte erst einige Fragen um zur Auswertung zu gelangen."
+const TEXT_MODAL_NOTHING_ANSWERED_TEXT = "Es wurden keine Fragen beantwortet. Bitte beantworten Sie erst einige Fragen um zur Auswertung zu gelangen."
 const TEXT_MODAL_NOTHING_ANSWERED_YES = "Wird erledigt!"
 const TEXT_MODAL_NOTHING_ANSWERED_NO = "Schließen"
 
-
+const TEXT_MODAL_CLIPBOARD_TITLE = "Fertig." 
+const TEXT_MODAL_CLIPBOARD_TEXT = "Die Internet-Adresse mit Ihren Ergebnissen wurde in die Zwischenablage kopiert."
+const TEXT_MODAL_CLIPBOARD_YES = "OK"
+const TEXT_MODAL_CLIPBOARD_NO = "Schließen"
